20cd3854cf83e5f53291240cb289c4659fcdbeed
Daniel <firepro20@gmail.com>
2021-02-23 11:26:11 +0100

    Imported place holder assets and created boilerplate

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LevelManager : Singleton<LevelManager>
+{
+    string fileName = "levelset1.txt";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadFromFile();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Parses through a level set text file
+    /// http://ianparberry.com/research/sokoban/
+    /// </summary>
+    void LoadFromFile()
+    {
+        StreamReader sr = new StreamReader(Application.dataPath + "/Data/LevelSets/" + fileName);
+        string fileContents = sr.ReadToEnd();
+        sr.Close();
+
+        string[] lines = fileContents.Split("\n"[0]);
+        foreach (string line in lines)
+        {
+            Debug.Log(line);
+        }
+    }
+}

09f7bb5e9b3e6eb0fd8c75c19aadf33e06296048
Thanik <thanik@gmail.com>
2021-02-27 00:50:34 +0000

    Added power obtained dialogue

--- Assets/Scripts/PartyNPC.cs
+++ Assets/Scripts/PartyNPC.cs
@@ -8,6 +8,15 @@ public class PartyNPC : MonoBehaviour
     public ColorDatabase characterColorsDatabase;
 
     private SpriteRenderer sprRenderer;
+    private string[] obtainedDialogue =
+    {
+        "MOVE",
+        "\"There's never two without three!\"\nSumboi has joined your party! This lad can ADD + 3 to the boxes.",
+        "\"How did I end up here? I can’t remember… But I can help you escape.\"\nMull T. Plyer has joined your party! This man can MULTIPLY x2.",
+        "\"Looks like we can help each other… I’m Powermann. Nice to meet ya\"\nPowermann has joined your party! This dude can SQUARE the box numbers!",
+        "DIVISION",
+        "\"Hey… are you stuck here, too? Wanna join forces?\"\nRay Seth has joined your party! This guy can RESET the value on the boxes. Press SPACE to switch party members!"
+    };
     void Start()
     {
         sprRenderer = GetComponent<SpriteRenderer>();
@@ -21,7 +30,7 @@ public class PartyNPC : MonoBehaviour
         {
             Player player = col.gameObject.GetComponent<Player>();
             Debug.Log("[PartyNPC] Power obtained: " + obtainablePower);
-            GameManager.Instance.gameUIController.ShowDialogue("Power " + obtainablePower + " Obtained!");
+            GameManager.Instance.gameUIController.ShowDialogue(obtainedDialogue[(int)obtainablePower]);
             player.UpdatePower(obtainablePower, true);
             Destroy(gameObject);
         }
